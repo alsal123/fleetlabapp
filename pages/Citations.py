4957a31ac1431204df31f962808fc6409579c30a
Ali Saleh <108489191+alsal123@users.noreply.github.com>
2023-07-25 18:55:53 -0400

    Create Citations.py
    Added citations page

---
+++ pages/Citations.py
@@ -0,0 +1,42 @@
+import streamlit as st
+
+st.title("Citations")
+
+# Citations for Superintendents - AZ
+st.subheader('Superintendents - AZ')
+st.write('- Navajo County: [Link](https://navajocountyaz.gov/Departments/Superintendent-of-Schools/Office-Staff)')
+st.write('- Cochise County: [Link](https://www.cochise.az.gov/484/School-Superintendent)')
+st.write('- Coconino County: [Link](https://coconino.az.gov/ccesa)')
+st.write('- Gila County: [Link](https://www.gilacountyaz.gov/government/school_superintendent/index.php)')
+st.write('- Graham County: [Link](https://www.graham.az.gov/201/School-Superintendent)')
+st.write('- Greenlee County: [Link](https://greenlee.az.gov/elected-officials/superintendent-of-schools/)')
+st.write('- La Paz County: [Link](https://www.co.la-paz.az.us/447/Education-Service-Agency)')
+st.write('- Mohave County: [Link](https://www.mohave.gov/ContentPage.aspx?id=130)')
+st.write('- Pima County: [Link](https://www.schools.pima.gov/about-us/meet-the-superintendent)')
+st.write('- Pinal County: [Link](https://www.pinalcso.org/vnews/display.v/ART/63dbe79e9afc8)')
+st.write('- Santa Cruz County: [Link](https://www.santacruzcountyaz.gov/294/Superintendent-of-Schools)')
+st.write('- Yuma County: [Link](https://www.yumasupt.org/about-us.html)')
+
+# Citations for Superintendents - Other States
+st.subheader('Superintendents - Other States')
+st.write('- Iowa: [Link](https://educateiowa.gov/document-type/iowa-public-school-district-superintendent-information)')
+st.write('- Colorado: [Link](https://www.cde.state.co.us/cdegen/educationdirectory)')
+st.write('- Nebraska: [Link](https://www.education.ne.gov/nebraska-public-schools-administrators-email-lists/)')
+st.write('- Massachusetts: [Link](https://www.massupt.org/membership/directory-of-members/)')
+st.write('- Illinois: [Link](https://www.isbe.net/Pages/Data-Analysis-Directories.aspx)')
+st.write('- Rhode Island: [Link](https://ride.ri.gov/students-families/ri-public-schools/school-directory)')
+st.write('- West Virginia: [Link](https://wvde.state.wv.us/ed_directory/)')
+st.write('- Vermont: [Link](https://www.vtvsa.org/-superintendents)')
+st.write('- Ohio: [Link](https://answers.cpl.org/faq/318744)')
+st.write('- Minnesota: [Link](https://public.education.mn.gov/MdeOrgView/districts/index)')
+
+    # Detailed Information
+st.subheader('Detailed Information')
+st.write('- Minnesota Department of Education: [Link](https://education.mn.gov/mde/index.htm)')
+st.write('- Arizona Department of Education: [Link](https://www.azed.gov/)')
+st.write('- Michigan Department of Education: [Link](https://www.michigan.gov/mde)')
+st.write('- Michigan School Data: [Link](https://www.mischooldata.org/financial-data-files/)')
+st.write('- National Center for Education Statistics: [Link](https://nces.ed.gov/ccd/districtsearch/index.asp)')
+st.write('- Iowa Annual Transportation Data: [Link](https://educateiowa.gov/documents/2021-2022-annual-transportation-data-iowa-public-schools)')
+st.write('- Iowa School Transportation Publications and Data: [Link](https://educateiowa.gov/gov/pk-12/school-transportation/transportation-publications-and-data)')
+st.write('- Iowa Department of Transportation Data: [Link](https://data.iowadot.gov/datasets/b8a51840ff744b87bd2935f716ff5c48_0/about)')
